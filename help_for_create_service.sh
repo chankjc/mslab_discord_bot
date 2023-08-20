7e716c8704428891271db6c3e74a1f6cc3b8f290
chan9988 <r11922032@csie.ntu.edu.tw>
2023-08-21 01:20:23 +0800

    update daemon setting

--- help_for_create_service.sh
+++ help_for_create_service.sh
@@ -4,4 +4,7 @@ file="mslab_discord_bot.service"
 py=$(which python)
 dir=$(pwd)
 echo [Service] > $file
-echo ExecStart=$py $dir/bot.py >> $file
+echo WorkingDirectory=$dir >> $file
+echo Environment=\"PATH=$PATH\" >> $file
+echo ExecStart=$dir/run.sh >> $file
+echo ExecStop=/bin/kill -2 \$MAINPID >> $file
